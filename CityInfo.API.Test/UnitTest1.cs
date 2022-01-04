4849ca743b86fbdebf56e59e5a3d207e0f1c8a6b
Georgios Voulgarakis <gvoulgarakis@microsoft.com>
2022-01-04 12:10:36 +0100

    Max-parallelism example; printing test run messages real-time;

--- CityInfo.API.Test/UnitTest1.cs
+++ CityInfo.API.Test/UnitTest1.cs
@@ -15,43 +15,5 @@ namespace CityInfo.API.Tests
         public void TestMethod1()
         {
         }
-
-        [TestMethod]
-        public async Task RxTaskParallelismTest()
-        {
-            await Observable.Range(1, 5)
-                .Select(i => Observable.Defer(() =>
-                {
-                    return TaskCreator(i).ToObservable();
-                }))
-                //Executed out-of-order tasks
-                .Merge(3) //If no args, no max-parallelism is specified. 
-                //.Concat() //Concat will execute the stream in-order
-                //.Do(x => Console.WriteLine($"{x} concatenated"))
-                .RunAsync(CancellationToken.None);
-                // .ForEachAsync(i => Console.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Got {i}"));
-        }
-
-        private Task<int> TaskCreator(int i)
-        {
-            return Task.Run(async () =>
-            {
-                Console.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Task{i}: Started {new string('.', i)}");
-                await Task.Delay(TimeSpan.FromSeconds(i));
-                //Thread.Sleep(TimeSpan.FromSeconds(i));
-                Console.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Task{i}: Complete {new string('.', i)}");
-
-                return i;
-            });
-        }
-
-        async Task<int> LongProcessAsync(int n)
-        {
-            Console.WriteLine($"Job {n} started");
-            await Task.Delay(TimeSpan.FromSeconds(5 - n));
-            Console.WriteLine($"Job {n} done");
-
-            return n;
-        }
     }
 }
---
+++ CityInfo.Chat.Test/UnitTest1.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+
+namespace CityInfo.Chat.Test;
+
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+public class Tests
+{
+    //https://www.lambdatest.com/blog/nunit-vs-xunit-vs-mstest/
+    [OneTimeSetUp]
+    public static void TestFixtureSetup()
+    {
+    }
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task RxTaskParallelismTest()
+    {
+        await Observable.Range(1, 10)
+            .Select(i => Observable.Defer(() =>
+            {
+                return TaskCreator(i).ToObservable();
+            }))
+            //Executed out-of-order tasks
+            .Merge(3) //If no args, no max-parallelism is specified. 
+            //.Concat() //Concat will execute the stream in-order
+            //.Do(x => TestContext.Progress.WriteLine($"{x} concatenated"))
+            .RunAsync(CancellationToken.None);
+        // .ForEachAsync(i => TestContext.Progress.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Got {i}"));
+        
+        //Printed at the end of the run
+        TestContext.Out.WriteLine("at the end");
+        // Immediately display error
+        TestContext.Error.WriteLine("immediate error");
+        // Immedaitely display message
+        TestContext.Progress.WriteLine("immediate text");
+    }
+
+    private Task<int> TaskCreator(int i)
+    {
+        return Task.Run(async () =>
+        {
+            TestContext.Progress.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Task{i}: Started {new string('.', i)}");
+            await Task.Delay(TimeSpan.FromSeconds(i));
+            //Thread.Sleep(TimeSpan.FromSeconds(i));
+            TestContext.Progress.WriteLine($"[{DateTime.UtcNow:hh:mm:ss.fff}] Task{i}: Complete {new string('.', i)}");
+
+            return i;
+        });
+    }
+}
